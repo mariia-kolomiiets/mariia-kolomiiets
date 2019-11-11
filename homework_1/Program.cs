ec6602c94323278e7a6737705cc10d8384829c1f
mariia-kolomiiets <54811905+mariia-kolomiiets@users.noreply.github.com>
2019-11-11 18:18:35 +0200

    Add files via upload

---
+++ homework_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string oper = "";
+            string first_n;
+            string second_n;
+            double first;
+            double second;
+            List<string> operation = new List<string>() { "+", "*", "/", "-" };
+            while (!operation.Contains(oper))
+            {
+                Console.Write("Operation: ");
+                oper = Console.ReadLine();
+            }
+            //Console.Write("Operation: ");
+            //first_n = Console.ReadLine();
+            //second_n = Console.ReadLine();
+            Console.Write("First number: ");
+            while (!Double.TryParse(first_n = Console.ReadLine(), out first))
+            {
+                Console.WriteLine("First number: ");
+                //first_n = Console.ReadLine();
+
+            }
+            Console.Write("Second number: ");
+            while (!Double.TryParse(second_n = Console.ReadLine(), out second))
+            {
+                Console.WriteLine("Second number: ");
+            }
+            if (oper == "+"){ Console.WriteLine("The answer is {0}", first + second); }
+            else if (oper == "-") { Console.WriteLine("The answer is {0}", first - second); }
+            else if (oper == "*") { Console.WriteLine("The answer is {0}", first * second); }
+            else { Console.WriteLine("The answer is {0}", first / second); }
+            Console.ReadKey();
+        }
+    }
+}
