aef87865af9e6601fd55e76cddda97bea7968714
mariia-kolomiiets <54811905+mariia-kolomiiets@users.noreply.github.com>
2019-12-03 18:12:03 +0200

    Add files via upload

---
+++ ModuleConrol_2/js/main.js
@@ -0,0 +1,20 @@
+var some_word = "Hi, this is simple sentence.";
+var some_another_word = "This is custom error!";
+var another_word = "Hi";
+
+var first_n = 8;
+var second_n = 9;
+
+var solution = first_n+second_n;
+var solution_2 = first_n*second_n;
+var solution_3 = first_n/second_n;
+var solution_4 = first_n-second_n;
+console.log(some_word);
+console.error(some_another_word);
+console.log("There is the weird thing in js. When i summ some string and integers. There will be string value.");
+console.error("Look: this is first number"+first_n+" and this is second"+second_n+" and result is: "+first_n+second_n);
+console.error("And this is wrong!");
+console.log("So i have to operate with them earlier. Summ of 8 and 9 will be: "+solution);
+console.log("8*9="+solution_2);
+console.log("8/9="+solution_3);
+console.log("8-9="+solution_4);
